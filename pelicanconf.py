e129fa0a4888f299c5e4a25af17f21dc89b09fa5
hiroya.kondo <kondou.hiroya@dirgroup.jp>
2024-04-16 15:16:34 +0900

    pelicanまわり追加

---
+++ pelicanconf.py
@@ -0,0 +1,77 @@
+AUTHOR = 'hiroya'
+SITENAME = 'TestEvidences'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Asia/Tokyo'
+
+DEFAULT_LANG = 'Japanese'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# カスタマイズ
+THEME = './themes/elegant'
+USE_FOLDER_AS_CATEGORY = True
+PLUGINS = ['pelican_just_table']
+JTABLE_TEMPLATE = """
+{% if bodies|length < 5 %}
+<div class="custom-table1">
+{% else %}
+<div class="custom-table2">
+{% endif %}
+<table class="example">
+    {% if caption %}
+    <caption> {{ caption }} </caption>
+    {% endif %}
+    {% if th != 0 %}
+    <thead>
+    <tr>
+        {% if ai == 1 %}
+        <th> No. </th>
+        {% endif %}
+        {% for head in heads %}
+        <th>{{ head }}</th>
+        {% endfor %}
+    </tr>
+    </thead>
+    {% endif %}
+    <tbody>
+        {% for body in bodies %}
+        <tr>
+            {% if ai == 1 %}
+            <td> {{ loop.index }} </td>
+            {% endif %}
+            {% for entry in body %}
+            <td>{{ entry }}</td>
+            {% endfor %}
+        </tr>
+        {% endfor %}
+    </tbody>
+</table>
+</div>
+"""
